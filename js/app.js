31a98ff00ead2206e7a546b89b4190a3b72c5089
rishikiran608-map <rishikiran608@gmail.com>
2026-07-11 21:22:05 +0530

    Deploy Blinkit-style weight badges, hide brand tag row on mobile to optimize height, and bump cache version to 1.6.0

--- js/app.js
+++ js/app.js
@@ -163,9 +163,6 @@ function renderProducts() {
           </div>
           <h3 class="product-title" title="${product.name}">${product.name}</h3>
           <span class="product-pack-size">
-            <svg style="width: 14px; height: 14px; fill: var(--text-light);" viewBox="0 0 24 24">
-              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-5 14H4v-4h11v4zm0-5H4V9h11v4zm5 5h-4V9h4v9z"/>
-            </svg>
             ${product.packSize}
           </span>
           <div class="product-pricing" style="margin-bottom: 1.15rem; margin-top: auto;">
